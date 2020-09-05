b3dce57b75447ff1891650ee36bec1ab007c3574
Agostina Gamboa Strauss <agostinags@gmail.com>
2020-09-05 19:19:00 -0300

    Ejercicio 1

--- src/com/learn/secuenciales/Ejercicio1.java
+++ src/com/learn/secuenciales/Ejercicio1.java
@@ -1,5 +1,27 @@
 package com.learn.secuenciales;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        // Leer un numero por teclado e imprimirlo en pantalla con el siguiente cartel: “Numero ingresado” = numero
+
+        Scanner miScanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero: ");
+
+        int numero = miScanner.nextInt();
+
+        System.out.println("Numero ingresado: "+numero);
+
+
+
+
+
+
+
+
+
+    }
 
 }
