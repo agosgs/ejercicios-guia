65de0ae7de1a8b4fa80d3386678e052b688b65c6
Agostina Gamboa Strauss <agostinags@gmail.com>
2020-09-05 19:56:43 -0300

    Ejercicio 1 ciclos repetitivos

---
+++ src/com/learn/repetitivas/Ejercicio1.java
@@ -0,0 +1,20 @@
+package com.learn.repetitivas;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+       // Imprimir por pantalla una lista de 20 numeros consecutivos los
+        //cuales comienzan con un numero ingresado por teclado
+
+        Scanner miScanner = new Scanner(System.in);
+        System.out.println("Ingresar un numero: ");
+
+        int numero = miScanner.nextInt();
+
+        for (int i = numero; i <=numero+20 ; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
