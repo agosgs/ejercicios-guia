f19869d613324e2af87901f02c533f3909fd902c
Agostina Gamboa Strauss <agostinags@gmail.com>
2020-09-18 16:58:19 -0300

    Ejercicio 6 selectivas

---
+++ src/com/learn/selectivas/Ejercio6.java
@@ -0,0 +1,21 @@
+package com.learn.selectivas;
+
+import java.util.Scanner;
+
+public class Ejercio6 {
+    //6) Escribir un algoritmo que determine si un número N es divisible por M.
+
+    public static void main(String[] args) {
+        Scanner miScanner = new Scanner(System.in);
+
+        System.out.println("Ingresar dos numeros: ");
+        int numeroN = miScanner.nextInt();
+        int numeroM = miScanner.nextInt();
+
+        if (numeroN%numeroM == 0){
+            System.out.println("El numero: "+numeroN + " es divisible por " + numeroM);
+        }else{
+            System.out.println("El numero: "+numeroN + " NO es divisible por " + numeroM);
+        }
+    }
+}
