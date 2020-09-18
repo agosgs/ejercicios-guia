1404b1ddd7a42feebdb144f4d75cbac7a7d7f3e0
Agostina Gamboa Strauss <agostinags@gmail.com>
2020-09-18 16:02:23 -0300

    Ejercicio 4 selectivas

---
+++ src/com/learn/selectivas/Ejercicio4.java
@@ -0,0 +1,21 @@
+package com.learn.selectivas;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    //4) Escribir un algoritmo que determine si un número es par.
+
+    public static void main(String[] args) {
+
+        Scanner miScanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero: ");
+
+        int numero = miScanner.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("El numero ingresado es par.");
+        } else {
+            System.out.println("El numero ingresado no es par.");
+        }
+    }
+}
