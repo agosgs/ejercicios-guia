15fee4b1ef3305da078fd93db0f93b2c1888fea0
Agostina Gamboa Strauss <agostinags@gmail.com>
2020-10-04 14:22:23 -0300

    Ejercicio 9 repetitivas

---
+++ src/com/learn/repetitivas/Ejercicio9.java
@@ -0,0 +1,48 @@
+package com.learn.repetitivas;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+    //9) Dada una serie de números enteros terminada en cero, imprimir los tres mayores.
+    public static void main(String[] args) {
+
+        Scanner miScanner = new Scanner(System.in);
+        System.out.println("Por favor, ingrese el tamanio de su array: ");
+
+        int tamanioArray = miScanner.nextInt();
+
+        int[] numerosEnteros = new int[tamanioArray];
+
+        for (int i = 0; i < numerosEnteros.length; i++) {
+            System.out.printf("Introduzca un número terminado en 0: ", i + 1);
+            numerosEnteros[i] = miScanner.nextInt();
+        }
+
+        boolean hayCambios = false;
+
+        int auxiliar = 0;
+
+        do{
+            hayCambios = false;
+            for(int i = 0 ; i<numerosEnteros.length-1; i++){
+                if(numerosEnteros[i+1] > numerosEnteros[i]){
+                    auxiliar = numerosEnteros[i];
+                    numerosEnteros[i] = numerosEnteros[i+1];
+                    numerosEnteros[i+1] = auxiliar;
+                    hayCambios=true;
+                }
+            }
+
+        }while(hayCambios);
+
+        for(int i = 0 ; i < numerosEnteros.length ; i++) {
+            //System.out.print(numerosEnteros[i]+", ");
+        }
+
+        int maximo = numerosEnteros[0];
+        int maximoDos = numerosEnteros[1];
+        int maximoTres = numerosEnteros[2];
+
+        System.out.println("Los tres numeros mayores del array son: "+ maximo + ", "+ maximoDos + " y " + maximoTres);
+    }
+}
