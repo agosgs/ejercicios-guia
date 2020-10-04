1aa2792b5e308dd455d1bafa51b52d4390ff2066
Agostina Gamboa Strauss <agostinags@gmail.com>
2020-10-03 22:06:51 -0300

    Ejercicio 6 repetitivas

---
+++ src/com/learn/repetitivas/Ejercicio6.java
@@ -0,0 +1,11 @@
+package com.learn.repetitivas;
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    //6) Leer A y B, enteros. Calcular C = A ∗ B mediante sumas sucesivas e imprimir el resultado.
+
+    public static void main(String[] args) {
+        Scanner miScanner = new Scanner(System.in);
+
+        System.out.println("Por favor, ingrese dos numeros enteros: ");
